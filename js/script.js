4977eff1a9d2c8ae57c8d25bc06ef300f77bdb68
Andreea <andreeafischler@gmail.com>
2022-10-15 23:04:07 +0300

    loader added to Milestone1

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ class Exchange {
         exchangeList.innerHTML = `${this.name} (${this.symbol})`
         return exchangeList
     }
+    
 }
 
 class ExchangeSearch {
@@ -20,42 +21,54 @@ class ExchangeSearch {
         const searchResult = document.getElementById('search-form')
         searchResult.addEventListener('submit', (e) => {
             e.preventDefault();
+            this.createSpinner();
             this.runExchangeSearch();
-            
         })
     }
 
    async runExchangeSearch() {
-   this.query = document.getElementById('search-input').value
-   const results = await this.getExchange();
+        this.query = document.getElementById('search-input').value
+        const results = await this.getExchange();
+        const container = document.getElementById('list-container')
+        
+        const exchangeObject = [];
 
-   const container = document.getElementById('list-container')
-   
-   const exchangeObject = [];
-   results.forEach((item) => {
-    const exchange = new Exchange(item)
-    exchangeObject.push(exchange);
-    const list = exchange.createExchangeList()
-    container.appendChild(list)
+        results.forEach((item) => {
+        const exchange = new Exchange(item)
+        exchangeObject.push(exchange);
+        const list = exchange.createExchangeList()
+        container.appendChild(list)
    }) 
     
   }
-  
+  createSpinner() {
+    const spinner = document.getElementById('spinner')
+    spinner.classList.add('spinner-grow', 'spinner-grow-sm')
+    
+}
+  removeSpinner(){
+    const spinner = document.getElementById('spinner');
+    spinner.classList.remove('spinner-grow', 'spinner-grow-sm')
+   }
   async getExchange() {
     try {
+        
         const url = 'https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/search?' + 'query=' + this.query + '&limit=' + this.limit + '&exchange=' + this.exchange
         const response = await fetch(url);
-        const results = await response.json();  
+        const results = await response.json();
+        this.removeSpinner()
         return results
     }
-  catch(e) {
+catch(e) {
    return []; 
 
   }
   
 }
+
  
 }
 
 const exchangeSearch = new ExchangeSearch()
 
+   
